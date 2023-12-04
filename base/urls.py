23e64f2ca31079f0c13d965d456b19effb90b014
JoshPham <JTPham911@gmail.com>
2023-12-04 01:33:09 +0000

    Post system, no forms yet

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('posts/', views.post, name="post")
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import PostItem
 
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
+
+def post(request):
+    context = PostItem.objects.all()
+    return render(request, "post.html", {'posts': context})
