a52d04c016c688faf531403b5ee93777979e1cf5
JoshPham <JTPham911@gmail.com>
2023-12-16 23:27:54 +0000

    added Grade, Unit, and Problem models to main app dir

--- base/admin.py
+++ base/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import Grade, Unit, Problem
+
+admin.site.register(Grade)
+admin.site.register(Unit)
+admin.site.register(Problem)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Grade(models.Model):
+    unit = models.name = models.ForeignKey('Unit', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, null=False)
+    description = models.TextField(null=True)
+    
+
+class Unit(models.Model):
+    problem = models.name = models.ForeignKey('Problem', on_delete=models.CASCADE)
+    unit_number = models.IntegerField()
+    title = models.CharField(max_length=50, null=False)
+    description = models.TextField(null=True)
+
+class Problem(models.Model):
+    question = models.CharField(max_length=50, null=True)
+    expression = models.CharField(max_length=20, null=False)
+    answer = models.IntegerField(blank=False, null=False)
