0575a62bf3b7fed82ac01625f34425f47022a8b3
JoshPham <JTPham911@gmail.com>
2023-12-17 02:10:55 +0000

    Reformated tutor models so they are associated using ForeignKey

--- tutor/models.py
+++ tutor/models.py
@@ -1,30 +1,29 @@
 from django.db import models
 
-# Create your models here.
 class Grade(models.Model):
     grade_id = models.IntegerField(default=3)
     title = models.CharField(max_length=50, null=False)
     description = models.TextField(null=True)
-    units = models.name = models.ManyToManyField("Unit", limit_choices_to={'grade_id__icontains': grade_id})
+    units = models.ManyToManyField("Unit", related_name="related_grades")
     
     def __str__(self):
         return f'{self.title}'
 
 class Unit(models.Model):
-    grade_id = models.IntegerField(default=3)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, related_name="related_units", default=1)
     unit_id = models.IntegerField()
     title = models.CharField(max_length=50, null=False)
     description = models.TextField(null=True)
-    problems = models.name = models.ManyToManyField("Problem", limit_choices_to={'unit_id__icontains': unit_id})
+    problems = models.ManyToManyField("Problem", related_name="related_units")
     
     def __str__(self):
-        return f'Unit {self.unit_id} - {self.title} (Grade {self.grade_id})'
+        return f'{self.title} ({self.grade.title})'
 
 class Problem(models.Model):
-    unit_id = models.IntegerField(default=1)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name="related_problems", default=1)
     question = models.CharField(max_length=50, null=True)
     expression = models.CharField(max_length=20, null=True)
     answer = models.IntegerField(blank=False, null=False)
     
     def __str__(self):
-        return f'How to solve {self.expression}({self.answer}) (Unit {self.unit_id})'
+        return f'{self.question} - {self.answer} (Unit {self.unit.unit_id}, {self.unit.grade.title})'
